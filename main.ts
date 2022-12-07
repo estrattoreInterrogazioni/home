9c1f8c95d900021953bdd14cc2f4a98bccf3f20b
Theramar <lorenzo1.cini@gmail.com>
2022-12-07 17:19:53 +0100

    monthButton

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-var today = new Date();
-
 var monthEl = <HTMLElement>document.getElementById("month");
 var agenda = <HTMLElement>document.getElementById("agenda");
 var titles = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName("title");
+var buttonLeft = <HTMLButtonElement>document.getElementById("btnMonthBefore");
+var buttonRight = <HTMLButtonElement>document.getElementById("btnMonthAfter");
 
 import {createAgenda} from "./code/agenda.js";
-createAgenda(agenda, titles, monthEl);
+
+createAgenda(agenda, monthEl, titles, buttonLeft, buttonRight);
